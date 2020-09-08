dc14610813bb9878c9b5a8b9e223b4c3e737cb73
Kevin <yinkai1205@163.com>
2020-09-08 09:22:27 +0800

    自定义校验规则

--- assets/js/login.js
+++ assets/js/login.js
@@ -10,4 +10,12 @@ $(function () {
     $(".login-box").show();
     $(".reg-box").hide();
   });
+
+  // 从 layui 中获取 form 对象
+  var form = layui.form;
+  // 通过 form.verify() 函数自定义校验规则
+  form.verify({
+    // 自定义了一个叫做 pwd 校验规则
+    pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+  });
 });
