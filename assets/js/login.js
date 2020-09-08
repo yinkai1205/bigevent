1b1d0f29d4e3e2014e6e1763c66512183abb4e84
Kevin <yinkai1205@163.com>
2020-09-08 09:25:39 +0800

    为注册表单设置校验规则

--- assets/js/login.js
+++ assets/js/login.js
@@ -17,5 +17,16 @@ $(function () {
   form.verify({
     // 自定义了一个叫做 pwd 校验规则
     pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+    // 校验两次密码是否一致的规则
+    repwd: function (value) {
+      // 通过形参拿到的是确认密码框中的内容
+      // 还需要拿到密码框中的内容
+      // 然后进行一次等于的判断
+      // 如果判断失败,则return一个提示消息即可
+      var pwd = $(".reg-box [name=password]").val();
+      if (pwd !== value) {
+        return "两次密码不一致！";
+      }
+    },
   });
 });
