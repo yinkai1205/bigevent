d53bbc9632c3d7329d06d1437a917d2f82809797
Kevin <yinkai1205@163.com>
2020-09-08 15:53:38 +0800

    获取用户的基本信息

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -8,4 +8,20 @@ $(function () {
       }
     }
   })
+
+  initUserInfo()
+
+  // 初始化用户的基本信息
+  function initUserInfo() {
+    $.ajax({
+      method: 'GET',
+      url: '/my/userinfo',
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('获取用户信息失败！')
+        }
+        console.log(res)
+      }
+    })
+  }
 })
