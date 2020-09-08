85b72a6026a06af9b5fe8d7d7110a621e444260a
Kevin <yinkai1205@163.com>
2020-09-08 09:54:26 +0800

    在ajaxPrefilter中统一拼接请求的根路径

--- assets/js/login.js
+++ assets/js/login.js
@@ -40,7 +40,7 @@ $(function () {
       username: $('#form_reg [name=username]').val(),
       password: $('#form_reg [name=password]').val(),
     }
-    $.post('http://ajax.frontend.itheima.net/api/reguser', data, function (
+    $.post('/api/reguser', data, function (
       res
     ) {
       if (res.status !== 0) {
@@ -57,7 +57,7 @@ $(function () {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
-      url: 'http://ajax.frontend.itheima.net/api/login',
+      url: '/api/login',
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
