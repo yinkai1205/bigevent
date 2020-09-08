4804b8984690a3fe30f2a7ffad67bc11ceee6765
Kevin <yinkai1205@163.com>
2020-09-08 16:04:04 +0800

    使用form.val方法快速为表单赋值

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -20,7 +20,9 @@ $(function () {
         if (res.status !== 0) {
           return layer.msg('获取用户信息失败！')
         }
-        console.log(res)
+        // console.log(res)
+        // 调用 `form.val()` 方法为表单赋值
+        form.val('formUserInfo', res.data)
       }
     })
   }
