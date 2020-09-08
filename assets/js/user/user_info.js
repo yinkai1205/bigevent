1b8268919584c5b605af04a123788e62e9d2a814
Kevin <yinkai1205@163.com>
2020-09-08 16:09:24 +0800

    发起请求更新用户的信息

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -33,4 +33,24 @@ $(function () {
     e.preventDefault()
     initUserInfo()
   })
+
+  // 监听表单的提交事件
+  $('.layui-form').on('submit', function (e) {
+    // 阻止表单的默认提交行为
+    e.preventDefault()
+    // 发起 ajax 数据请求
+    $.ajax({
+      method: 'POST',
+      url: '/my/userinfo',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('更新用户信息失败！')
+        }
+        layer.msg('更新用户信息成功！')
+        // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+        window.parent.getUserInfo()
+      }
+    })
+  })
 })
