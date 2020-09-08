db7907cd511f99a44b549f5eed2e175da9550de3
Kevin <yinkai1205@163.com>
2020-09-08 14:14:33 +0800

    实现退出功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,23 @@
 $(function () {
   // 调用 getUserInfo 获取用户基本信息
   getUserInfo()
+
+  var layer = layui.layer
+
+  // 点击按钮，实现退出功能
+  $('#btnLogout').on('click', function () {
+    // 提示用户是否确认退出
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+      //do something
+      // 1. 清空本地存储中的 token
+      localStorage.removeItem('token')
+      // 2. 重新跳转到登录页面
+      location.href = '/login.html'
+
+      // 关闭 confirm 询问框
+      layer.close(index)
+    })
+  })
 })
 
 // 获取用户的基本信息
@@ -42,3 +59,4 @@ function renderAvatar(user) {
       .show()
   }
 }
+
