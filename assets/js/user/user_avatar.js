f494641197631517731fb76b0bfbdc7c261b0f6f
Kevin <yinkai1205@163.com>
2020-09-08 17:02:26 +0800

    将裁剪后的头像上传到服务器

--- assets/js/user/user_avatar.js
+++ assets/js/user/user_avatar.js
@@ -35,4 +35,31 @@ $(function () {
       .attr('src', imgURL) // 重新设置图片路径
       .cropper(options) // 重新初始化裁剪区域
   })
+
+  // 为确定按钮，绑定点击事件
+  $('#btnUpload').on('click', function () {
+    // 1. 要拿到用户裁剪之后的头像
+    var dataURL = $image
+      .cropper('getCroppedCanvas', {
+        // 创建一个 Canvas 画布
+        width: 100,
+        height: 100
+      })
+      .toDataURL('image/png') // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+    // 2. 调用接口，把头像上传到服务器
+    $.ajax({
+      method: 'POST',
+      url: '/my/update/avatar',
+      data: {
+        avatar: dataURL
+      },
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('更换头像失败！')
+        }
+        layer.msg('更换头像成功！')
+        window.parent.getUserInfo()
+      }
+    })
+  })
 })
