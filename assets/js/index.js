c1c417555ac1c8b2a1161bec16ec0d63beee5c05
Kevin <yinkai1205@163.com>
2020-09-08 14:05:25 +0800

    渲染用户头像

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,7 +13,32 @@ function getUserInfo() {
       Authorization: localStorage.getItem('token') || ''
     },
     success: function (res) {
-      console.log(res)
+      // console.log(res)
+      // 调用 renderAvatar 渲染用户的头像
+      renderAvatar(res.data)
     }
   })
+}
+
+// 渲染用户的头像
+function renderAvatar(user) {
+  // 1. 获取用户的名称
+  var name = user.nickname || user.username
+  // 2. 设置欢迎的文本
+  $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+  // 3. 按需渲染用户的头像
+  if (user.user_pic !== null) {
+    // 3.1 渲染图片头像
+    $('.layui-nav-img')
+      .attr('src', user.user_pic)
+      .show()
+    $('.text-avatar').hide()
+  } else {
+    // 3.2 渲染文本头像
+    $('.layui-nav-img').hide()
+    var first = name[0].toUpperCase()
+    $('.text-avatar')
+      .html(first)
+      .show()
+  }
 }
