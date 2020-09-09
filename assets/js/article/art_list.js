1bce9eb01ad025583b30fd83253b663b906bce36
Kevin <yinkai1205@163.com>
2020-09-09 23:48:26 +0800

    定义美化时间格式的过滤器

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,6 +1,26 @@
 $(function () {
   var layer = layui.layer
 
+  // 定义美化时间的过滤器
+  template.defaults.imports.dataFormat = function (date) {
+    const dt = new Date(date)
+
+    var y = dt.getFullYear()
+    var m = padZero(dt.getMonth() + 1)
+    var d = padZero(dt.getDate())
+
+    var hh = padZero(dt.getHours())
+    var mm = padZero(dt.getMinutes())
+    var ss = padZero(dt.getSeconds())
+
+    return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+  }
+
+  // 定义补零的函数
+  function padZero(n) {
+    return n > 9 ? n : '0' + n
+  }
+
   // 定义一个查询的参数对象，将来请求数据的时候，
   // 需要将请求参数对象提交到服务器
   var q = {
@@ -18,7 +38,7 @@ $(function () {
       method: 'GET',
       url: '/my/article/list',
       data: q,
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('获取文章列表失败！')
         }
