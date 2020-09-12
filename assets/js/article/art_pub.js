383c363267f67b5753f90468db39b82eae1215cf
Kevin <yinkai1205@163.com>
2020-09-12 23:01:48 +0800

    发起Ajax请求实现发布文章的功能

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -87,6 +87,28 @@ $(function () {
         // 5. 将文件对象，存储到 fd 中
         fd.append('cover_img', blob)
         // 6. 发起 ajax 数据请求
+        publishArticle(fd)
       })
   })
+
+  // 定义一个发布文章的方法
+  function publishArticle(fd) {
+    $.ajax({
+      method: 'POST',
+      url: '/my/article/add',
+      data: fd,
+      // 注意：如果向服务器提交的是 FormData 格式的数据，
+      // 必须添加以下两个配置项
+      contentType: false,
+      processData: false,
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('发布文章失败！')
+        }
+        layer.msg('发布文章成功！')
+        // 发布文章成功后，跳转到文章列表页面
+        location.href = '/article/art_list.html'
+      }
+    })
+  }
 })
