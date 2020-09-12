2c96f4333fb167e83fe644fa59e01fd0b07474e1
Kevin <yinkai1205@163.com>
2020-09-12 22:06:35 +0800

    渲染封面裁剪区域

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -23,4 +23,17 @@ $(function () {
       }
     })
   }
+
+  // 1. 初始化图片裁剪器
+  var $image = $('#image')
+
+  // 2. 裁剪选项
+  var options = {
+    aspectRatio: 400 / 280,
+    preview: '.img-preview'
+  }
+
+  // 3. 初始化裁剪区域
+  $image.cropper(options)
+
 })
