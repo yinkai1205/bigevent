c78cf487f84e5fbb9fc2543c9ac3ab253a8fc7c2
Kevin <yinkai1205@163.com>
2020-09-12 22:26:14 +0800

    将裁剪后的封面追加到FormData对象中

--- assets/js/article/art_pub.js
+++ assets/js/article/art_pub.js
@@ -74,9 +74,19 @@ $(function () {
     var fd = new FormData($(this)[0])
     // 3. 将文章的发布状态，存到 fd 中
     fd.append('state', art_state)
-
-    fd.forEach(function (v, k) {
-      console.log(k, v)
-    })
+    // 4. 将封面裁剪过后的图片，输出为一个文件对象
+    $image
+      .cropper('getCroppedCanvas', {
+        // 创建一个 Canvas 画布
+        width: 400,
+        height: 280
+      })
+      .toBlob(function (blob) {
+        // 将 Canvas 画布上的内容，转化为文件对象
+        // 得到文件对象后，进行后续的操作
+        // 5. 将文件对象，存储到 fd 中
+        fd.append('cover_img', blob)
+        // 6. 发起 ajax 数据请求
+      })
   })
 })
