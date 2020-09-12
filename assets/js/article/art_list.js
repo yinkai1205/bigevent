5a16905f89eb4662662b13ffd9c327ae12526533
Kevin <yinkai1205@163.com>
2020-09-12 21:44:42 +0800

    实现页码值-1的操作

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -143,6 +143,11 @@ $(function () {
           // 4
           if (len === 1) {
             // 如果 len 的值等于1，证明删除完毕之后，页面上就没有任何数据了
+            // 页码值最小必须是 1
+            // if (q.pagenum > 1) {
+            //   q.pagenum--
+            // }
+            q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
           }
           initTable()
         }
